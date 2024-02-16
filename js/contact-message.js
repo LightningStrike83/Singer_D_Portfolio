85c4716ef87a0b88ec4e5ff0183dc0f401b52d77
Dan Singer <d_singer@fanshaweonline.ca>
2024-02-15 19:40:45 -0500

    Contact Upgrade Attempt
    Tried to upgrade the contact script

---
+++ js/contact-message.js
@@ -0,0 +1,59 @@
+export function contactMessage() {
+    const contactForm = document.querySelector("#contact-d")
+    const feedback = document.querySelector("#feedback-area")
+    const messageConfirm = document.querySelector("#confirm-send")
+
+    console.log("Boo")
+
+    function contactFeedback(event) {
+        event.preventDefault();
+        const url = "contact-form.php"
+        const thisform = this
+
+        
+
+        const formData =
+        "pname=" + thisform.elements.preferred_name.value +
+        "&email=" + thisform.elements.email.value +
+        "&subject=" + thisform.elements.subject.value +
+        "&message=" + thisform.elements.comments.value;
+
+        console.log(formData)
+
+        fetch (url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: formData
+        })
+
+        .then(response => response.json())
+        .then(responseText => {
+            feedback.innerHTML = "";
+
+            if (responseText.errors) {
+                responseText.errors.forEach(error => {
+                    const errorText = document.createElement("p")
+                    errorText.textContent = error
+                    feedback.appendChild(errorText);
+                })
+            } else {
+                contactForm.reset();
+                const sendConfirm = document.createElement("p")
+                sendConfirm.textContent = responseText.message
+                messageConfirm.appendChild(sendConfirm)
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            feedback.innerHTML = ""
+            const errorMessage = document.createElement("p")
+
+            errorMessage.textContent = "Sorry, your browser doesn't seem to be support AJAX. Apologies!"
+
+            feedback.appendChild(errorMessage)
+        })
+    }
+    contactForm.addEventListener("submit", contactFeedback)
+}
--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,4 @@
 export function contact() {
-    const sendButton = document.querySelector("#submit-contact")
-
-    function openCustomMessage() {
-        const messageBox = document.querySelector("#contact-popup")
-
-        messageBox.style.display = "grid"
-    }
-
-    sendButton.addEventListener('click', openCustomMessage)
+    
 }
     
